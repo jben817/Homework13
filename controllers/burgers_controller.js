fd7c1a57522040b47e3cd6aafc7dc5f35bef51ed
j.bennett <61255945+jben817@users.noreply.github.com>
2020-06-03 20:50:13 -0400

    added api routes on burgers_controller.js

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,5 +1,44 @@
 var express = require("express");
+var burger = require("../models/burger");
+
+var express = require("express");
 var router = express.Router();
 
 // Import the model (cat.js) to use its database functions.
 var burger = require("../models/burger");
+
+router.get("/", function (req, res) {
+  burger.selectAll(function (data) {
+    var hbsObject = {
+      burgers: data,
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
+
+router.post("/api/burgers", function (req, res) {
+  var userBurgerInfo = req.body;
+
+  burger.insertOne(req.body, function (result) {
+    // Send back the ID of the new quote
+    res.json({ id: result.insertId });
+  });
+});
+
+router.put("/api/burgers/:id", function (req, res) {
+  var condition = { id: req.params.id };
+
+  console.log("condition", condition);
+
+  burger.updateOne(condition, function (result) {
+    if (result.changedRows == 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
+
+module.exports = router;
