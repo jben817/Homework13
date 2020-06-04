c4cabec4e91b7c88cb08bac29eea0d4029a10dd6
j.bennett <61255945+jben817@users.noreply.github.com>
2020-06-03 21:21:49 -0400

    updated orm.js

--- config/orm.js
+++ config/orm.js
@@ -1,6 +1,5 @@
 var connection = require("./connection");
 
-
 // * In the `orm.js` file, create the methods that will execute the necessary MySQL commands in the controllers. These are the methods you will need to use in order to retrieve and store data in your database.
 
 //      * `selectAll()`
@@ -10,39 +9,45 @@ var connection = require("./connection");
 //    * Export the ORM object in `module.exports`.
 
 var orm = {
-    selectAll: function(whatToSelect, tableInput) {
-      var queryString = "SELECT ?? FROM ??";
-      connection.query(queryString, [whatToSelect, tableInput], function(err, result) {
+  selectAll: function (tableInput, cb) {
+    var queryString = "SELECT * FROM ??";
+    connection.query(queryString, [tableInput], function (err, result) {
+      if (err) throw err;
+      console.log(result);
+      cb(result);
+    });
+  },
+  insertOne: function (tableInput, userInfo, cb) {
+    var queryString = "INSERT INTO ?? SET ?";
+    //{burger_name: "cheese", devoured: false}
+
+    console.log(queryString);
+
+    connection.query(queryString, [tableInput, userInfo], function (
+      err,
+      result
+    ) {
+      if (err) throw err;
+      console.log(result);
+      cb(result);
+    });
+  },
+  updateOne: function (tableInput, whatToUpdate, whereToUpdate, cb) {
+    var queryString = "UPDATE ?? SET ? WHERE ?";
+    //{id: 5}
+
+    console.log(queryString);
+
+    connection.query(
+      queryString,
+      [tableInput, whatToUpdate, whereToUpdate],
+      function (err, result) {
         if (err) throw err;
         console.log(result);
-      });
-    },
-    insertOne: function(tableInput, colToSearch, valOfCol) {
-      var queryString = "SELECT * FROM ?? WHERE ?? = ?";
-  
-      console.log(queryString);
-  
-      connection.query(queryString, [tableInput, colToSearch, valOfCol], function(err, result) {
-        if (err) throw err;
-        console.log(result);
-      });
-    },
-    updateOne: function(whatToSelect, tableOne, tableTwo, onTableOneCol, onTableTwoCol) {
-      var queryString = "SELECT ?? FROM ?? AS tOne";
-      queryString += " LEFT JOIN ?? AS tTwo";
-      queryString += " ON tOne.?? = tTwo.??";
-  
-      console.log(queryString);
-  
-      connection.query(queryString, [whatToSelect, tableOne, tableTwo, onTableOneCol, onTableTwoCol], function(
-        err,
-        result
-      ) {
-        if (err) throw err;
-        console.log(result);
-      });
-    }
-  };
-  
-  module.exports = orm;
-  
+        cb(result);
+      }
+    );
+  },
+};
+
+module.exports = orm;
